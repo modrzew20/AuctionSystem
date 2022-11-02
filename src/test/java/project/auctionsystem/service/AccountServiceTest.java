b4b11623ef3bb4b093b053eee4b255b400901b45
229963 Daniel Modrzejewski <229963@edu.p.lodz.pl>
2022-11-02 22:04:05 +0100

    service tests (mockito)

---
+++ src/test/java/project/auctionsystem/service/AccountServiceTest.java
@@ -0,0 +1,123 @@
+package project.auctionsystem.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.auctionsystem.entity.AccessLevel;
+import project.auctionsystem.entity.Account;
+import project.auctionsystem.entity.Auction;
+import project.auctionsystem.exception.AccessLevelNotFoundException;
+import project.auctionsystem.exception.AccountNotFoundException;
+import project.auctionsystem.repository.AccessLevelRepository;
+import project.auctionsystem.repository.AccountRepository;
+import project.auctionsystem.repository.AuctionRepository;
+import project.auctionsystem.service.impl.AccountServiceImpl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@SpringBootTest
+@Transactional
+class AccountServiceTest {
+
+
+    @Mock
+    AccountRepository accountRepository;
+
+    @Mock
+    AccessLevelRepository accessLevelRepository;
+
+    @Mock
+    AuctionRepository auctionRepository;
+
+    @InjectMocks
+    AccountServiceImpl accountService;
+
+    private Account account;
+    private AccessLevel accessLevel;
+    private final static String USERNAME = "username";
+    private final static String PASSWORD = "password";
+
+    @BeforeEach
+    void init() {
+        accessLevel = new AccessLevel();
+        accessLevel.setName(AccessLevel.CLIENT);
+
+        account = new Account();
+        account.setUsername(USERNAME);
+        account.setPassword(PASSWORD);
+        account.setAccessLevel(accessLevel);
+    }
+
+    @Test
+    void getAllAccountsTest() {
+        List<Account> list = new ArrayList<>();
+        list.add(account);
+        when(accountRepository.findAll()).thenReturn(list);
+
+        List<Account> result = accountService.getAll();
+        assertEquals(list.size(), result.size());
+    }
+
+    @Test
+    void getAccountTest() throws AccountNotFoundException {
+        when(accountRepository.findById(USERNAME)).thenReturn(Optional.of(account));
+
+        Account result = accountService.get(USERNAME);
+        assertEquals(account, result);
+    }
+
+    @Test
+    void getAccountNotFoundException() {
+        when(accountRepository.findById(USERNAME)).thenReturn(Optional.empty());
+        assertThrows(AccountNotFoundException.class, () -> accountService.get(USERNAME));
+    }
+
+    @Test
+    void createAccountTest() throws AccessLevelNotFoundException {
+        when(accessLevelRepository.findById(AccessLevel.CLIENT)).thenReturn(Optional.of(accessLevel));
+        when(accountRepository.save(account)).thenReturn(account);
+
+        Account result = accountService.create(account);
+        assertEquals(account, result);
+    }
+
+    @Test
+    void createAccountAccessLevelNotFoundException() {
+        when(accessLevelRepository.findById(AccessLevel.CLIENT)).thenReturn(Optional.empty());
+        assertThrows(AccessLevelNotFoundException.class, () -> accountService.create(account));
+    }
+
+    @Test
+    void getBalanceTest() {
+
+        List<Auction> auctionList = new ArrayList<>();
+
+        Random random = new Random();
+        int randomInt = random.nextInt(30);
+
+        for (int i = 0; i < randomInt; i++) {
+            Auction auction = new Auction();
+            auction.setPrice(100.0);
+            auction.setWinner(account);
+            auction.setEndDate(LocalDateTime.now().minusDays(1));
+            auctionList.add(auction);
+        }
+        when(auctionRepository.findByWinnerUsernameAndEndDateBefore(any(String.class), any(LocalDateTime.class))).thenReturn(auctionList);
+
+        double result = accountService.getBalance(USERNAME, "usd");
+        assertEquals(randomInt * 100.0, result);
+    }
+
+}
---
+++ src/test/java/project/auctionsystem/service/AuctionServiceTest.java
@@ -0,0 +1,120 @@
+package project.auctionsystem.service;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+import project.auctionsystem.entity.Account;
+import project.auctionsystem.entity.Auction;
+import project.auctionsystem.exception.AuctionExpiredException;
+import project.auctionsystem.exception.AuctionNotFoundException;
+import project.auctionsystem.exception.InvalidEndDateProvidedException;
+import project.auctionsystem.exception.InvalidPriceProvidedException;
+import project.auctionsystem.repository.AuctionRepository;
+import project.auctionsystem.service.impl.AuctionServiceImpl;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+@Transactional
+class AuctionServiceTest {
+
+    @Mock
+    AuctionRepository auctionRepository;
+    @InjectMocks
+    AuctionServiceImpl auctionService;
+
+    private final static String TITLE = "title";
+    private final static Double PRICE = 100.0;
+    private final static LocalDateTime END_DATE = LocalDateTime.now().plusDays(2);
+
+    private Auction auction;
+
+    @BeforeEach
+    void init() {
+        Account account = new Account();
+
+        auction = new Auction();
+        auction.setTitle(TITLE);
+        auction.setWinner(account);
+        auction.setPrice(PRICE);
+        auction.setEndDate(END_DATE);
+    }
+
+    @Test
+    void getAllAuctionTest() {
+        List<Auction> list = new ArrayList<>();
+        list.add(auction);
+        when(auctionRepository.findAll()).thenReturn(list);
+
+        List<Auction> result = auctionService.getAll();
+        assertEquals(result.size(), list.size());
+    }
+
+    @Test
+    void getAuctionTest() throws AuctionNotFoundException {
+        when(auctionRepository.findById(auction.getId())).thenReturn(java.util.Optional.of(auction));
+
+        Auction result = auctionService.get(auction.getId());
+        assertEquals(result.getTitle(), auction.getTitle());
+    }
+
+    @Test
+    void getAuctionThrowNotFoundExceptionTest() {
+        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.empty());
+        assertThrows(AuctionNotFoundException.class, () -> auctionService.get(auction.getId()));
+    }
+
+    @Test
+    void createAuctionTest() throws InvalidEndDateProvidedException {
+        when(auctionRepository.save(auction)).thenReturn(auction);
+
+        Auction result = auctionService.create(auction);
+        assertEquals(result.getTitle(), auction.getTitle());
+        verify(auctionRepository, times(1)).save(auction);
+    }
+
+    @Test
+    void createAuctionInvalidEndDateProvidedExceptionText() {
+        auction.setEndDate(LocalDateTime.now().minusDays(1));
+        assertThrows(InvalidEndDateProvidedException.class, () -> auctionService.create(auction));
+    }
+
+    @Test
+    void updatePriceTest() throws AuctionExpiredException, AuctionNotFoundException, InvalidPriceProvidedException {
+        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
+        when(auctionRepository.save(auction)).thenReturn(auction);
+
+        Auction result = auctionService.updatePrice(auction.getId(), 200.0);
+        assertEquals(200.0, result.getPrice());
+        verify(auctionRepository, times(1)).save(auction);
+    }
+
+    @Test
+    void updatePriceAuctionExpiredExceptionTest() {
+        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
+        auction.setEndDate(LocalDateTime.now().minusDays(1));
+        assertThrows(AuctionExpiredException.class, () -> auctionService.updatePrice(auction.getId(), 200.0));
+    }
+
+    @Test
+    void updatePriceAuctionNotFoundExceptionTest() {
+        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.empty());
+        assertThrows(AuctionNotFoundException.class, () -> auctionService.updatePrice(auction.getId(), 200.0));
+    }
+
+    @Test
+    void updatePriceInvalidPriceProvidedExceptionTest() {
+        when(auctionRepository.findById(auction.getId())).thenReturn(Optional.of(auction));
+        assertThrows(InvalidPriceProvidedException.class, () -> auctionService.updatePrice(auction.getId(), 50.0));
+    }
+}
